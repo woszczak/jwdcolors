bd76a22d767a586d398d6b3167b25527c58ac2ed
Justyna Woszczak <justynawoszczak1@gmail.com>
2023-06-04 10:59:31 +0200

    Konfiguracja dostępu do bazy danych

--- src/main/java/pl/edu/wszib/jwd/jwdcolors/JwdcolorsApplication.java
+++ src/main/java/pl/edu/wszib/jwd/jwdcolors/JwdcolorsApplication.java
@@ -2,7 +2,10 @@ package pl.edu.wszib.jwd.jwdcolors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+
+@PropertySource("classpath:db.properties") //wskazuje gdzie jest plik z ustawieniami
 @SpringBootApplication
 public class JwdcolorsApplication {
 
