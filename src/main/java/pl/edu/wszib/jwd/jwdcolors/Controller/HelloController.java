f5e05dcc11e8c264715e1a2f708741bab7590817
Justyna Woszczak <justynawoszczak1@gmail.com>
2023-06-04 10:03:14 +0200

    Prosty kontroler restowy do testu

---
+++ src/main/java/pl/edu/wszib/jwd/jwdcolors/Controller/HelloController.java
@@ -0,0 +1,17 @@
+package pl.edu.wszib.jwd.jwdcolors.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("hello")
+    public String Hello(){
+        return "Hello!!!";
+    };
+
+
+}
